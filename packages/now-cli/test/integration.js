0525c9fb4c12e95fb7eff589ad53f2a31417f783
Max Leiter <maxwell.leiter@gmail.com>
2020-06-16 12:55:05 -0700

    Re-add console logs

--- packages/now-cli/test/integration.js
+++ packages/now-cli/test/integration.js
@@ -535,6 +535,7 @@ test('Deploy `api-env` fixture and test `vercel env` command', async t => {
     const { host } = new URL(stdout);
 
     const apiUrl = `https://${host}/api/get-env`;
+    console.log({ apiUrl });
     const apiRes = await fetch(apiUrl);
     t.is(apiRes.status, 200, formatOutput({ stderr, stdout }));
     const apiJson = await apiRes.json();
@@ -542,11 +543,13 @@ test('Deploy `api-env` fixture and test `vercel env` command', async t => {
     t.is(apiJson['VERCEL_URL'], host);
 
     const homeUrl = `https://${host}`;
+    console.log({ homeUrl });
+
     const homeRes = await fetch(homeUrl);
     t.is(homeRes.status, 200, formatOutput({ stderr, stdout }));
     const homeJson = await homeRes.json();
     t.is(homeJson['MY_ENV_VAR'], 'MY_VALUE');
-    t.is(homeJson['VERCEL_URL'], host);
+    t.is(homeJson['VERCEL_URL'], '');
   }
 
   async function nowDevAndFetchCloudVars() {
